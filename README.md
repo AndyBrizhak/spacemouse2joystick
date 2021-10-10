6169ff13c25cf2b06da8ddf95b3a295cfd2651f2
Logan Power <lmpower2@illinois.edu>
2021-10-09 23:21:04 -0500

    Added code and updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # spacemouse2joystick
-Linux Python script that translates input from a SpaceMouse into a virtual joystick
+This Python script reads data in from a 3DConnexion SpaceNavigator and creates a virtual joystick device through uinput.
+As it uses uinput to create the virtual device, this script is only compatible with Linux.
+
+## Dependencies
+* python-uinput
+* approxeng.input
+Both of these can be installed through pip.
+
+## Setup
+You must first ensure that the uinput kernel module is loaded:
+```
+modprobe uinput
+```
+In order to be able to run this script as anything other than the root user, you will need to additionally do the following things:
+1. Determine which group owns the `eventx` files located in `/dev/input`.
+2. Add yourself to that group: `sudo usermod -aG group username`
+3. Create a group "uinput": `sudo groupadd uinput`
+3. Add [this udev rule](https://github.com/tuomasjjrasanen/python-uinput/blob/master/udev-rules/40-uinput.rules) to your udev rules.
+4. Add yourself to the uinput group: `sudo usermod -aG uinput username`
+5. Reboot.
+
+After rebooting, you should be able to run this script as a normal user/without sudo.
+
+## Usage
+This script takes no arguments.  You can run it in the background of a terminal window using any method you like.  I prefer using good ol' `nohup` to run mine in the background and allow me to close the terminal window.
+
+Have fun gaming!
---
+++ sm2joy.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  9 19:49:51 2021
+
+@author: lpower
+"""
+
+from time import sleep
+from approxeng.input.selectbinder import ControllerResource
+from approxeng.input.spacemousepro import SpaceMousePro
+from approxeng.input.controllers import find_matching_controllers, ControllerRequirement
+import uinput
+
+# Monkeypatch the library's idea of a spacemouse
+def new_registration_ids():
+    return [(0x46d, 0xc62b), (0x46d, 0xC626)]
+
+SpaceMousePro.registration_ids = staticmethod(new_registration_ids)
+
+events = (
+    uinput.BTN_0,
+    uinput.BTN_1,
+    uinput.ABS_X + (-350, 350, 0, 0),
+    uinput.ABS_Y + (-350, 350, 0, 0),
+    uinput.ABS_Z + (-350, 350, 0, 0),
+    uinput.ABS_RX + (-350, 350, 0, 0),
+    uinput.ABS_RY + (-350, 350, 0, 0),
+    uinput.ABS_RZ + (-350, 350, 0, 0),
+    )
+
+scale = 350
+
+while True:
+    try:
+        with ControllerResource() as sm:
+            with uinput.Device(events) as device:
+                print('Found a SpaceMouse and connected to it.')
+                last_z = 0
+                while sm.connected:
+                    # Do processing
+                    # presses = sm.check_presses()
+                    # if sm.has_presses:
+                    #     if 'fit' in presses:
+                    #         device.emit_click(uinput.BTN_0)
+                    #         #print("BTN0")
+                    #     if 'menu' in presses:
+                    #         device.emit_click(uinput.BTN_1)
+                    #         #print("BTN1")
+                    b1, b0 = sm['fit', 'menu']
+                    if b0 is not None:
+                        device.emit(uinput.BTN_0, 1)
+                    else:
+                        device.emit(uinput.BTN_0, 0)
+                    if b1 is not None:
+                        device.emit(uinput.BTN_1, 1)
+                    else:
+                        device.emit(uinput.BTN_1, 0)
+                    device.emit(uinput.ABS_X, int(sm.lx * scale), syn=False)
+                    device.emit(uinput.ABS_Y, int(sm.ly * scale), syn=False)
+                    device.emit(uinput.ABS_Z, int(sm.lz * scale), syn=False)
+                    # Pretty sure these axes are called something different from what they are
+                    device.emit(uinput.ABS_RX, int(sm.roll * scale), syn=False)
+                    device.emit(uinput.ABS_RY, int(sm.pitch * scale), syn=False)
+                    device.emit(uinput.ABS_RZ, int(sm.yaw * scale), syn=True)
+        print('Connection to SpaceMouse lost')
+    except IOError:
+        print("Unable to find a SpaceMouse yet!")
+        sleep(1.0)
